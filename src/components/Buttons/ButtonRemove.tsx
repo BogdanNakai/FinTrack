4dd2059006fb11ae9da86345cc37747ee0f57e88
Bogdan <nakaibogdan@gmail.com>
2026-07-24 12:29:54 +0300

    refacto: add type to the button remove

--- src/components/Buttons/ButtonRemove.tsx
+++ src/components/Buttons/ButtonRemove.tsx
@@ -1,4 +1,6 @@
-const ButtonRemove = () => {
+import type { IButtonProps } from "./Buttons.type";
+
+const ButtonRemove = ({ textButton }: IButtonProps) => {
   return (
     <>
       <div>
@@ -6,7 +8,7 @@ const ButtonRemove = () => {
           type="button"
           className="block tracking-[0.02em] px-[20px] box-border h-[40px] min-w-[120px] bg-[#EF4444] text-[16px] text-[#FFFFFF] rounded-[8px] hover:bg-[#f87171] active:scale-[0.95] transition-[0.15s]"
         >
-          Remove
+          {textButton}
         </button>
       </div>
     </>
