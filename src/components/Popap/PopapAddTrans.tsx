94163ecafca077eea904bd2357281c181d9fd170
Bogdan <nakaibogdan@gmail.com>
2026-07-27 13:02:11 +0300

    refacto: rewrote everything using components

--- src/components/Popap/PopapAddTrans.tsx
+++ src/components/Popap/PopapAddTrans.tsx
@@ -1,3 +1,6 @@
+import ButtonPrimary from "../Buttons/ButtonPrimary";
+import ButtonSecondary from "../Buttons/ButtonSecondary";
+import InputPopap from "../Form/InputPopap";
 import SelectFilter from "../SelectFilter/SelectFilter";
 
 const PopapaddTrans = () => { 
@@ -23,23 +26,13 @@ const PopapaddTrans = () => {
       <h3 className="text-2xl pb-5">Add Transaction</h3>
       <form className="h-auto grid grid-cols-1 gap-3.5">
         <label className="relative flex items-center">
-          <input
-            type="date"
-            className="absolute left-2.5 w-5 h-5 overflow-hidden"
-          />
-          <input
-            type="text"
-            placeholder="Title"
-            className="h-11 bg-white w-full rounded-xl pl-9 outline-[#2684FF] border-2 border-[#e2e8f0]"
-          />
+          <InputPopap type="date" name="Title" placeholder="Title" />
         </label>
         <label className="block w-40 h-10">
-          <input
+          <InputPopap
             type="text"
             name="Description"
             placeholder="Enter Description"
-            id=""
-            className="h-11 bg-white w-full rounded-xl pl-3 outline-[#2684FF] border-2 border-[#e2e8f0]"
           />
         </label>
         <label className="w-full">
@@ -52,27 +45,11 @@ const PopapaddTrans = () => {
           <SelectFilter options={typeValue} titlePlaceholder="All Types" />
         </label>
         <label className="w-21">
-          <input
-            type="number"
-            name=""
-            placeholder="Amount"
-            id=""
-            className="h-11 pl-3 bg-white w-full rounded-xl outline-[#2684FF] border-2 border-[#e2e8f0]"
-          />
+          <InputPopap type="number" name="Amount" placeholder="Amount" />
         </label>
         <div className="flex justify-end items-center gap-3">
-          <button
-            type="button"
-            className="w-35 h-10 text-[#00b894] border-1 border-solid border-[#00b894] rounded-2xl"
-          >
-            Cancel
-          </button>
-          <button
-            type="button"
-            className="w-35 h-10 bg-[#00b894]  rounded-2xl text-white"
-          >
-            Add
-          </button>
+          <ButtonSecondary textButton="Cansel" />
+         <ButtonPrimary textButton="Add"/>
         </div>
       </form>
     </div>
