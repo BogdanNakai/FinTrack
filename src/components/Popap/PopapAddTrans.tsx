ef250e7f309716faa644d4bd630e082317e7fa79
Bogdan <nakaibogdan@gmail.com>
2026-07-28 14:41:45 +0300

    refacto: chenge popap addTrasaction

--- src/components/Popap/PopapAddTrans.tsx
+++ src/components/Popap/PopapAddTrans.tsx
@@ -1,10 +1,10 @@
 import ButtonPrimary from "../Buttons/ButtonPrimary";
 import ButtonSecondary from "../Buttons/ButtonSecondary";
+import InputData from "../Form/InputData";
 import InputPopap from "../Form/InputPopap";
 import SelectFilter from "../SelectFilter/SelectFilter";
 
 const PopapaddTrans = () => { 
-
   const categoriesValue = [
     { value: "savings-account", label: "Savings Account" },
     { value: "health-fitness", label: "Health & Fitness" },
@@ -20,36 +20,38 @@ const PopapaddTrans = () => {
     { value: "income", label: "Income" },
     { value: "expense", label: "Expense" },
   ];
-
 	return (
     <div className="w-160 h-107.5 p-6 bg-[#ffffff] rounded-xl absolute top-50% visible border-2 border-[#000]">
       <h3 className="text-2xl pb-5">Add Transaction</h3>
       <form className="h-auto grid grid-cols-1 gap-3.5">
         <label className="relative flex items-center">
-          <InputPopap type="date" name="Title" placeholder="Title" />
+          <InputData />
         </label>
         <label className="block w-40 h-10">
           <InputPopap
-            type="text"
             name="Description"
             placeholder="Enter Description"
+            type="text"
           />
         </label>
         <label className="w-full">
           <SelectFilter
-            options={categoriesValue}
-            titlePlaceholder="All Category"
+            SelectOptionsList={categoriesValue}
+            NameSelect={"All Category"}
           />
         </label>
         <label className="w-full">
-          <SelectFilter options={typeValue} titlePlaceholder="All Types" />
+          <SelectFilter
+            SelectOptionsList={typeValue}
+            NameSelect={"All Types"}
+          />
         </label>
-        <label className="w-21">
+        <label className="max-w-full w-40">
           <InputPopap type="number" name="Amount" placeholder="Amount" />
         </label>
         <div className="flex justify-end items-center gap-3">
           <ButtonSecondary textButton="Cansel" />
-         <ButtonPrimary textButton="Add"/>
+          <ButtonPrimary textButton="Add" />
         </div>
       </form>
     </div>
