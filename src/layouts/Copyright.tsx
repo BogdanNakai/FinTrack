adc4acb1c1424fa0408c70722506f97904de6391
Bogdan <nakaibogdan@gmail.com>
2026-07-30 15:49:00 +0300

    feat: create copyright layout

---
+++ src/layouts/Copyright.tsx
@@ -0,0 +1,30 @@
+const Copyright = () => {
+  return (
+    <section className="border-t-1 border-[#e2e8f0]">
+      <div className="copyright__container">
+        <div className="flex justify-between items-center py-[23px] text-[#64748B] ">
+          <p className="text-[12px]">© 2025 FinTrack. All rights reserved.</p>
+          <ul className="flex gap-2.5">
+            <li>
+              <a href="" className="block p-2.6 text-[14px]">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="" className="block p-2.6 text-[14px]">
+                Terms of Use
+              </a>
+            </li>
+            <li>
+              <a href="" className="block p-2.6 text-[14px]">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Copyright;
