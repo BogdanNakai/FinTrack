b9c0f0c233d54288ceacd4fb4c4b1f129da075e7
Bogdan <nakaibogdan@gmail.com>
2026-08-07 19:25:33 +0300

    feat: create new inputs

--- src/components/Form/InputMonthe.tsx
+++ src/components/Form/InputMonthe.tsx
@@ -3,12 +3,13 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import type { Iinput } from "./Form.type";
 
-function InputData({ placeholder }: Iinput) {
+
+function InputMonthe({ placeholder }: Iinput) {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DatePicker
-        label={placeholder}
-        format="DD MMMM YYYY"
+        label={'"month" and "year"'}
+        views={["month", "year"]}
         slotProps={{
           field: {
             openPickerButtonPosition: "start",
@@ -36,4 +37,4 @@ function InputData({ placeholder }: Iinput) {
   );
 }
 
-export default InputData;
+export default InputMonthe;
