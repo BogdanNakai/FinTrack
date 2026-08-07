5ac15f434bc20aaf2c88204d1e0756c6f4448423
Bogdan <nakaibogdan@gmail.com>
2026-08-07 19:24:50 +0300

    feat: create new button

---
+++ src/components/Buttons/ButtonPrimaryAtion.tsx
@@ -0,0 +1,17 @@
+import type { IButtonProps } from "./Buttons.type";
+import plus from '@/assets/icon_plus.svg'
+
+const ButtonPrimaryAtion = ({ textButton, link }: IButtonProps) => {
+  return (
+    <a
+      href={link}
+      type="button"
+      className="flex justify-center items-center gap-[15px] rounded-[8px] tracking-[0.02em] px-[20px] box-border min-h-[40px] min-w-[120px] items-center bg-[#00B894] text-white hover:bg-[#00DCA0] active:scale-[0.95] active:bg-[#BEEBD8] transition-[0.15s] border-[1px] border-[#e2e8f0]"
+    >
+      <img src={plus} alt="Image" />
+      {textButton}
+    </a>
+  );
+};
+
+export default ButtonPrimaryAtion;
