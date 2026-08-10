e8d2cdf6f9f6bd780ec0674312a0a0765124e9cd
Bogdan <nakaibogdan@gmail.com>
2026-08-10 13:43:21 +0300

    refacto:Center action button...

--- src/components/Buttons/ButtonActions.tsx
+++ src/components/Buttons/ButtonActions.tsx
@@ -3,7 +3,7 @@ import editIcon from "@/assets/icon_change.svg";
 
 const ButtonActions = () => {
   return (
-    <span className="flex gap-1.5">
+    <span className="flex justify-center gap-1.5">
       <button
         type="button"
         className="bg-[#F5C644] w-9 h-9 rounded-full flex justify-center items-center"
