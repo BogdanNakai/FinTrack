78f5ece5a6930e09b1ea056d0d1d5dc3d5168f38
Bogdan <nakaibogdan@gmail.com>
2026-08-12 19:36:56 +0300

    fix: add media point

--- src/components/Card/DashboardCard/DashboardCard.tsx
+++ src/components/Card/DashboardCard/DashboardCard.tsx
@@ -11,7 +11,7 @@ const DashboardCard = ({ title, balanse, growth }) => {
         className="flex flex-col justify-start items-start flex-[0_1_150px] sm:flex-[0_1_150px] lg:flex-[0_1_260px] min-w-[150px] md:min-w-[190px] w-full relative overflow-hidden gap-2 p-2.5 md:p-4 rounded-xl bg-white"
         style={{ boxShadow: "0px 6px 18px 0 rgba(2,6,23,0.06)" }}
       >
-        <p className="flex-grow-0 flex-shrink-1 text-[10px] text-xs text-left text-slate-500">
+        <p className="flex-grow-0 flex-shrink-1 text-[10px] md:text-xs text-left text-slate-500">
           {title}
         </p>
         <p className="flex-grow-0 flex-shrink-1 text-[22px] md:text-[28px] font-medium text-left text-slate-800">
