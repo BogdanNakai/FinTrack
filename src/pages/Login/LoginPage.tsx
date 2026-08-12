f42befa67fd25a94388deed8ca9f960d638fcaf5
Bogdan <nakaibogdan@gmail.com>
2026-08-12 20:29:44 +0300

    fix: chenge viev login and sing in

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,5 +1,3 @@
-
-
 import google from "@/assets/icon_google.svg";
 import email from "@/assets/icon_message_input.svg";
 import password from "@/assets/icon_password_input.svg";
@@ -10,55 +8,55 @@ import InputPassword from "@/components/Form/InputPassword";
 const LoginPage = () => {
   return (
     <>
-      <section className="h-[100vh]">
-        <div className="max-w-7xl mx-auto h-[100%]">
-          <div className="md:flex md:flex-row md:h-[100%]  h-[50%] items-center">
-            <div className="flex-[0_0_60%] gap-[20px] h-[100%] flex flex-col justify-center">
-              <p className="text-[32px] text-center font-bold text-[#333]">
-                Sign in to FinTrack
-              </p>
-              <div className="flex justify-center">
-                <img src={google} alt="Image" />
-              </div>
-              <p className="text-[12px] text-center">or use your account</p>
-              <div className="flex justify-center">
-                <form action="" className="w-[320px]">
-                  <div className="grid gap-[20px] place-items-center">
-                    <Input type="email" placeholder="Email" icon={email} />
-                    <InputPassword
-                      name="input-password"
-                      placeholder="Password"
-                      icon={password}
-                    />
-                    <div>
-                      <a
-                        href=""
-                        className="text-[#64748B] text-[14px] text-center"
-                      >
-                        Forgot your password?
-                      </a>
+      <main>
+        <section className="min-[768px]:flex overflow-hidden h-full">
+          <div className="flex-[0_0_100%]">
+            <div className="max-w-[800px] h-full mx-auto px-3.5">
+              <div className="min-[768px]:w-[60%] gap-[20px] h-[100%] py-14 flex flex-col justify-center">
+                <p className="text-[32px] text-center font-bold text-[#333]">
+                  Sign in to FinTrack
+                </p>
+                <div className="flex justify-center">
+                  <img src={google} alt="Image" />
+                </div>
+                <p className="text-[12px] text-center">or use your account</p>
+                <div className="flex justify-center">
+                  <form action="" className="w-[320px]">
+                    <div className="grid gap-[20px] place-items-center">
+                      <Input type="email" placeholder="Email" icon={email} />
+                      <InputPassword
+                        name="input-password"
+                        placeholder="Password"
+                        icon={password}
+                      />
+                      <div>
+                        <a
+                          href=""
+                          className="text-[#64748B] text-[14px] text-center"
+                        >
+                          Forgot your password?
+                        </a>
+                      </div>
+                      <div className="flex justify-center w-[160px]">
+                        <ButtonPrimary textButton={"SIGN IN"} />
+                      </div>
                     </div>
-                    <div className="flex justify-center w-[160px]">
-                      <ButtonPrimary textButton={"SIGN IN"} />
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-            <div className="flex-[0_0_40%] gap-[12px] pr-[20px] pl-[20px] bg-[linear-gradient(332.9deg,_#185a4d_29.38%,_#4dd0b1_100%)] h-[100%] flex items-center flex-col justify-center">
-              <h2 className="text-4xl text-white text-center">
-                Hello, Friend!
-              </h2>
-              <p className="text-[14px] leading-[143%] text-white text-center ">
-                Enter your personal details and start journey with us
-              </p>
-              <div className="flex justify-center w-[160px]">
-                <ButtonPrimary link={ '/singIn'} textButton={"SIGN UP"} />
+                  </form>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+          <div className="min-[768px]:flex-[0_0_40%] min-[768px]:-translate-x-full max-[768px]:h-[500px] max-w-full px-[20px] bg-[linear-gradient(332.9deg,_#185a4d_29.38%,_#4dd0b1_100%)] max-h-[100%] flex items-center gap-[12px] flex-col justify-center">
+            <h2 className="text-4xl text-white text-center">Hello, Friend!</h2>
+            <p className="text-[14px] leading-[143%] text-white text-center ">
+              Enter your personal details and start journey with us
+            </p>
+            <div className="flex justify-center w-[160px]">
+              <ButtonPrimary link={"/singIn"} textButton={"SIGN UP"} />
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   );
 };
--- src/pages/SingIn/SingInPage.tsx
+++ src/pages/SingIn/SingInPage.tsx
@@ -1,5 +1,3 @@
-
-
 import google from "@/assets/icon_google.svg";
 import email from "@/assets/icon_message_input.svg";
 import password from "@/assets/icon_password_input.svg";
@@ -11,53 +9,55 @@ import InputPassword from "@/components/Form/InputPassword";
 const SingInPage = () => {
   return (
     <>
-      <section className="h-[100vh]">
-        <div className="max-w-7xl mx-auto h-[100%]">
-          <div className="md:flex md:flex-row md:h-[100%]  h-[50%] items-center">
-            <div className="flex-[0_0_40%] gap-[12px] pr-[20px] pl-[20px] bg-[linear-gradient(332.9deg,_#185a4d_29.38%,_#4dd0b1_100%)] h-[100%] flex items-center flex-col justify-center">
-              <h2 className="text-4xl text-white text-center">Welcome Back!</h2>
-              <p className="text-[14px] leading-[143%] text-white text-center ">
-                Log in to manage your finances.
-              </p>
-              <div className="flex justify-center w-[160px]">
-                <ButtonPrimary link={"/login"} textButton={"SIGN IN"} />
-              </div>
+      <main>
+        <section className="min-[768px]:flex overflow-hidden h-full">
+          <div className="min-[768px]:flex-[0_0_40%]  max-[768px]:h-[500px] max-w-full px-[20px] bg-[linear-gradient(332.9deg,_#185a4d_29.38%,_#4dd0b1_100%)] max-h-[100%] flex items-center gap-[12px] flex-col justify-center">
+            <h2 className="text-4xl text-white text-center">Welcome Back!</h2>
+            <p className="text-[14px] leading-[143%] text-white text-center ">
+              Log in to manage your finances.
+            </p>
+            <div className="flex justify-center w-[160px]">
+              <ButtonPrimary link={"/login"} textButton={"SIGN IN"} />
             </div>
-            <div className="flex-[0_0_60%] gap-[20px] h-[100%] flex flex-col justify-center">
-              <p className="text-[32px] text-center font-bold text-[#333]">
-                Create Account
-              </p>
-              <div className="flex justify-center">
-                <img src={google} alt="Image" />
-              </div>
-              <p className="text-[12px] text-center">
-                or use your email for registration
-              </p>
-              <div className="flex justify-center">
-                <form action="" className="w-[320px]">
-                  <div className="grid gap-[20px] place-items-center">
-                    <Input type="name" placeholder="Name" icon={user} />
-                    <Input type="email" placeholder="Email" icon={email} />
-                    <InputPassword
-                      name="input-password"
-                      placeholder="Password"
-                      icon={password}
-                    />
-                    <InputPassword
-                      name="input-password-confirm"
-                      placeholder="Confirm Password"
-                      icon={password}
-                    />
-                    <div className="flex justify-center w-[160px]">
-                      <ButtonPrimary textButton={"SIGN UP"} />
+          </div>
+          <div className="flex-1">
+            <div className="max-w-[800px] flex justify-center h-full mx-auto px-3.5">
+              <div className="w-full max-w-[500px]  gap-[20px] h-[100%] py-14 flex flex-col justify-center">
+                <p className="text-[32px] text-center font-bold text-[#333]">
+                  Create Account
+                </p>
+                <div className="flex justify-center">
+                  <img src={google} alt="Image" />
+                </div>
+                <p className="text-[12px] text-center">
+                  or use your email for registration
+                </p>
+                <div className="flex justify-center">
+                  <form action="" className="w-[320px]">
+                    <div className="grid gap-[20px] place-items-center">
+                      <Input type="name" placeholder="Name" icon={user} />
+                      <Input type="email" placeholder="Email" icon={email} />
+                      <InputPassword
+                        name="input-password"
+                        placeholder="Password"
+                        icon={password}
+                      />
+                      <InputPassword
+                        name="input-password-confirm"
+                        placeholder="Confirm Password"
+                        icon={password}
+                      />
+                      <div className="flex justify-center w-[160px]">
+                        <ButtonPrimary textButton={"SIGN UP"} />
+                      </div>
                     </div>
-                  </div>
-                </form>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </main>
     </>
   );
 };
