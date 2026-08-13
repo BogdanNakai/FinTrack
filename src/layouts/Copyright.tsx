d7b5d18f6aa9b4e414a16630391400b8dc1d176a
Bogdan <nakaibogdan@gmail.com>
2026-08-14 00:21:47 +0300

    refacto: add visual efect for link

--- src/layouts/Copyright.tsx
+++ src/layouts/Copyright.tsx
@@ -6,17 +6,26 @@ const Copyright = () => {
           <p className="text-[12px]">© 2025 FinTrack. All rights reserved.</p>
           <ul className="flex gap-2.5">
             <li>
-              <a href="" className="block p-2.6 text-[14px]">
+              <a
+                href=""
+                className="block p-2.6 text-[14px] transition duration-200 hover:text-[#64748b81] focus:text-[#64748b81] active:scale-[0.9] "
+              >
                 Privacy Policy
               </a>
             </li>
             <li>
-              <a href="" className="block p-2.6 text-[14px]">
+              <a
+                href=""
+                className="block p-2.6 text-[14px] transition duration-200 hover:text-[#64748b81] focus:text-[#64748b81] active:scale-[0.9]"
+              >
                 Terms of Use
               </a>
             </li>
             <li>
-              <a href="" className="block p-2.6 text-[14px]">
+              <a
+                href=""
+                className="block p-2.6 text-[14px] transition duration-200 hover:text-[#64748b81] focus:text-[#64748b81] active:scale-[0.9]"
+              >
                 Contact
               </a>
             </li>
