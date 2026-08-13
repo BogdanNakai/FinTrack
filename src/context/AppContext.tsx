9987c30bbe0289cd5e61eff27c32c8170191ce6a
Bogdan <nakaibogdan@gmail.com>
2026-08-13 14:40:30 +0300

    test: add new data for test

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -8,6 +8,9 @@ export const categoriesValue = [
   { value: "transport", label: "Transport" },
   { value: "salary", label: "Salary" },
   { value: "food-dining", label: "Food & Dining" },
+  { value: "entertainment", label: "Entertainment" },
+  { value: "education", label: "Education" },
+  { value: "others", label: "Others" },
 ];
 
 export const statusGoal = [
@@ -37,19 +40,6 @@ export const categoriesData = [
   { label: "Shopping", value: 13.1, color: "#ef4444", darkColor: "#b91c1c" },
   { label: "Others", value: 21.7, color: "#64748b", darkColor: "#334155" },
 ];
-
-export const lastFiveMonthe = (startDate = new Date()) => {
-  const months = [""];
-  for (let i = 5; i >= 0; i--) {
-    // цикл у зворотному порядку, щоб місяці йшли зліва направо хронологічно
-    const d = new Date(startDate.getFullYear(), startDate.getMonth() - i, 1);
-
-    // 'short' замість 'long' поверне скорочення з 3 літер (Jan, Feb, Mar...)
-    const monthName = d.toLocaleString("en-US", { month: "short" });
-    months.push(monthName);
-  }
-  return months; // Поверне, наприклад: ['Mar', 'Apr', 'May', 'Jun', 'Jul']
-};
 
 export const totalСostsСategory = [
   { value: 35000, label: "Food & Dining", color: "#FBBF24" },
@@ -99,3 +89,139 @@ export const seriesBudgetSpending = [
   { data: dataBudget, label: "Budget", color: "#3366CC" },
   { data: dataSpending, label: "Spending ", color: "#DC3912" },
 ];
+
+export const listTransactionBudget = [
+  {
+    categories: "Food & Dining",
+    limit: 100,
+    spent: 50,
+    remaining: 1000,
+  },
+  {
+    categories: "Savings Account",
+    limit: 10000,
+    spent: 8300,
+    remaining: 1000,
+  },
+  {
+    categories: "Shopping",
+    limit: 10000,
+    spent: 80300,
+    remaining: 1000,
+  },
+];
+
+export const listTransaction = [
+  {
+    dateValue: new Date(),
+    categories: "Investment",
+    description: "Lorem lorem lorem",
+    amount: 2000,
+    type: "Expense",
+  },
+  {
+    dateValue: new Date(),
+    categories: "Savings Account",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, aspernatur.",
+    amount: 43000,
+    type: "Income",
+  },
+  {
+    dateValue: new Date(),
+    categories: "Bills & Utilities",
+    description: "Lorem . Lorem ipsum dolor s",
+    amount: 1223000,
+    type: "Expense",
+  },
+  {
+    dateValue: new Date(),
+    categories: "Salary",
+    description: "Lorem ipsum dolor sit amet. Lorem ipsum dolor s",
+    amount: 200,
+    type: "Income",
+  },
+  {
+    dateValue: new Date(),
+    categories: "Salary",
+    description: "Lorem ipsum dolor sit amet. Lorem ipsum dolor s",
+    amount: 200,
+    type: "Income",
+  },
+  {
+    dateValue: new Date(),
+    categories: "Salary",
+    description: "Lorem ipsum dolor sit amet. Lorem ipsum dolor s",
+    amount: 200,
+    type: "Income",
+  },
+  {
+    dateValue: new Date(),
+    categories: "Salary",
+    description: "Lorem ipsum dolor sit amet. Lorem ipsum dolor s",
+    amount: 200,
+    type: "Income",
+  },
+  {
+    dateValue: new Date(),
+    categories: "Salary",
+    description: "Lorem ipsum dolor sit amet. Lorem ipsum dolor s",
+    amount: 200,
+    type: "Income",
+  },
+  {
+    dateValue: new Date(),
+    categories: "Salary",
+    description: "Lorem ipsum dolor sit amet. Lorem ipsum dolor s",
+    amount: 200,
+    type: "Income",
+  },
+  {
+    dateValue: new Date(),
+    categories: "Salary",
+    description: "Lorem ipsum dolor sit amet. Lorem ipsum dolor s",
+    amount: 200,
+    type: "Income",
+  },
+  {
+    dateValue: new Date(),
+    categories: "Salary",
+    description: "Lorem ipsum dolor sit amet. Lorem ipsum dolor s",
+    amount: 200,
+    type: "Income",
+  },
+  {
+    dateValue: new Date(),
+    categories: "Salary",
+    description: "Lorem ipsum dolor sit amet. Lorem ipsum dolor s",
+    amount: 200,
+    type: "Income",
+  },
+];
+
+export const herroBalans = [
+  {
+    totalBalans: 10000,
+    totalIncome: 400,
+    totalExpense: 510,
+  },
+];
+
+export const monyFormatter = new Intl.NumberFormat("en-IN", {
+  style: "currency",
+  currency: "INR",
+  maximumFractionDigits: 0,
+});
+
+export const lastFiveMonthe = (startDate = new Date()) => {
+  const months = [""];
+  for (let i = 5; i >= 0; i--) {
+    // цикл у зворотному порядку, щоб місяці йшли зліва направо хронологічно
+    const d = new Date(startDate.getFullYear(), startDate.getMonth() - i, 1);
+
+    // 'short' замість 'long' поверне скорочення з 3 літер (Jan, Feb, Mar...)
+    const monthName = d.toLocaleString("en-US", { month: "short" });
+    months.push(monthName);
+  }
+  return months; // Поверне, наприклад: ['Mar', 'Apr', 'May', 'Jun', 'Jul']
+};
